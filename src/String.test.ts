f751775229a557daced0b4651783f803cf7bcff9
apademide <contact@apademide.ch>
2023-11-12 16:25:33 +0100

    feat: S.(from|capitalize|to*Case|splitWords|trim*|pad*|truncate*) + tests

--- src/String.test.ts
+++ src/String.test.ts
@@ -3,6 +3,39 @@ import { S } from "~/String";
 import { describe, expect, test } from "@jest/globals";
 
 describe("S class", () => {
+  test("conversion to string works", () => {
+    expect(S.from("foo")).toBe("foo");
+    expect(S.from(new String("foo"))).toBe("foo");
+    expect(S.from(0)).toBe("0");
+    expect(S.from(new Number(0))).toBe("0");
+    expect(S.from(false)).toBe("false");
+    expect(S.from(new Boolean(false))).toBe("false");
+
+    expect(S.from({})).toBe("[object Object]");
+
+    expect(
+      S.from({
+        toString() {
+          return "foo";
+        },
+      })
+    ).toBe("foo");
+
+    expect(
+      S.from({
+        [Symbol.toPrimitive]() {
+          return 0;
+        },
+      })
+    ).toBe("0");
+
+    expect(S.from([])).toBe("");
+    expect(S.from([1, 2, 3])).toBe("1,2,3");
+
+    expect(S.from(null)).toBe("");
+    expect(S.from(undefined)).toBe("");
+  });
+
   test("non-strict typecheck works", () => {
     // Primitive strings and String objects should both pass the check.
     expect(S.is("string")).toBe(true);
@@ -36,5 +69,195 @@ describe("S class", () => {
     expect(S.isStrict([])).toBe(false);
     expect(S.isStrict(null)).toBe(false);
     expect(S.isStrict(undefined)).toBe(false);
+  });
+
+  test("splitWords() works", () => {
+    {
+      const string = "This is a string.";
+      const expected = ["This", "is", "a", "string"];
+      const split = S.splitWords(string);
+
+      expect(split).toEqual(expected);
+    }
+
+    {
+      const string = "ThisIsAString.";
+      const expected = ["This", "Is", "A", "String"];
+      const split = S.splitWords(string);
+
+      expect(split).toEqual(expected);
+    }
+
+    {
+      const string = "CAPS";
+      const expected = ["C", "A", "P", "S"];
+      const split = S.splitWords(string);
+
+      expect(split).toEqual(expected);
+    }
+  });
+
+  test("capitalize() works", () => {
+    expect(S.capitalize("foo")).toBe("Foo");
+    expect(S.capitalize("FooBar")).toBe("FooBar");
+    expect(S.capitalize("0")).toBe("0");
+  });
+
+  test("toTitleCase() works", () => {
+    expect(S.toTitleCase("this is a string")).toBe("This Is A String");
+    expect(S.toTitleCase("thisIsAString")).toBe("ThisIsAString");
+    expect(S.toTitleCase("The brand FOOBAR is the best!")).toBe(
+      "The Brand FOOBAR Is The Best!"
+    );
+    expect(S.toTitleCase("jean-claude van damme")).toBe(
+      "Jean-Claude Van Damme"
+    );
+  });
+
+  test("toLowerCase() works", () => {
+    expect(S.toLowerCase("FOO")).toBe("foo");
+    expect(S.toLowerCase("foo")).toBe("foo");
+    expect(S.toLowerCase("Foo")).toBe("foo");
+    expect(S.toLowerCase(0)).toBe("0");
+  });
+
+  test("toUpperCase() works", () => {
+    expect(S.toUpperCase("FOO")).toBe("FOO");
+    expect(S.toUpperCase("foo")).toBe("FOO");
+    expect(S.toUpperCase("Foo")).toBe("FOO");
+  });
+
+  const locales = ["en-US", "en-GB", "fr-FR", "fr-CA", "TR"];
+  const localizable = [
+    "I",
+    "İ",
+    "ı",
+    "İ",
+    "i",
+    "I",
+    "ß",
+    "SS",
+    "ss",
+    "istanbul",
+    "İstanbul",
+    "Gesäß",
+    "GESÄSS",
+  ];
+
+  test("toLocaleLowerCase() works", () => {
+    for (const locale of locales) {
+      for (const str of localizable) {
+        expect(S.toLocaleLowerCase(str, locale)).toBe(
+          str.toLocaleLowerCase(locale)
+        );
+      }
+    }
+  });
+
+  test("toLocaleUpperCase() works", () => {
+    for (const locale of locales) {
+      for (const str of localizable) {
+        expect(S.toLocaleUpperCase(str, locale)).toBe(
+          str.toLocaleUpperCase(locale)
+        );
+      }
+    }
+  });
+
+  test("trim() works", () => {
+    expect(S.trim(" \r foo \n\t")).toBe("foo");
+    expect(S.trim(" foo")).toBe("foo");
+    expect(S.trim("foo ")).toBe("foo");
+    expect(S.trim("foo")).toBe("foo");
+
+    expect(S.trim("foo", "f")).toBe("oo");
+    expect(S.trim("foo", "o")).toBe("f");
+    expect(S.trim("foo", "fo")).toBe("");
+    expect(S.trim("foo", "a")).toBe("foo");
+
+    expect(S.trim("yolollololo", /lo/)).toBe("yolol");
+    expect(S.trim("tatatatafalatatata", /tatata/)).toBe("tafala");
+    expect(S.trim("lolooolyolollololo", /[lo]/)).toBe("y");
+
+    // @ts-expect-error
+    expect(() => S.trim("foo", {})).toThrow(TypeError);
+  });
+
+  test("trimStart() works", () => {
+    expect(S.trimStart("\r\r\t foo ")).toBe("foo ");
+    expect(S.trimStart(" foo")).toBe("foo");
+    expect(S.trimStart("foo ")).toBe("foo ");
+    expect(S.trimStart("foo")).toBe("foo");
+
+    expect(S.trimStart("foo", "f")).toBe("oo");
+    expect(S.trimStart("foo", "o")).toBe("foo");
+
+    expect(S.trimStart("foo", "fo")).toBe("");
+    expect(S.trimStart("foo", "a")).toBe("foo");
+
+    expect(S.trimStart("lolololooooooyolollolo", /lo/)).toBe("oooooyolollolo");
+    expect(S.trimStart("tatatatatafalatata", /tatata/)).toBe("tatafalatata");
+    expect(S.trimStart("lolololooooooyolollololo", /[lo]/)).toBe("yolollololo");
+
+    // @ts-expect-error
+    expect(() => S.trimStart("foo", {})).toThrow(TypeError);
+  });
+
+  test("trimEnd() works", () => {
+    expect(S.trimEnd(" foo \r\t")).toBe(" foo");
+    expect(S.trimEnd(" foo")).toBe(" foo");
+    expect(S.trimEnd("foo ")).toBe("foo");
+    expect(S.trimEnd("foo")).toBe("foo");
+
+    expect(S.trimEnd("foo", "f")).toBe("foo");
+    expect(S.trimEnd("foo", "o")).toBe("f");
+
+    expect(S.trimEnd("foo", "fo")).toBe("");
+    expect(S.trimEnd("foo", "a")).toBe("foo");
+
+    expect(S.trimEnd("yolollololo", /lo/)).toBe("yolol");
+    expect(S.trimEnd("ratatatatatatata", /tatata/)).toBe("rata");
+    expect(S.trimEnd("yolollololo", /[lo]/)).toBe("y");
+
+    // @ts-expect-error
+    expect(() => S.trimEnd("foo", {})).toThrow(TypeError);
+  });
+
+  test("padStart() works", () => {
+    expect(S.padStart("foo", 3)).toBe("foo");
+    expect(S.padStart("foo", 2)).toBe("foo");
+
+    expect(S.padStart("foo", 5, ".")).toBe("..foo");
+    expect(S.padStart("foo", 5, "123")).toBe("12foo");
+  });
+
+  test("padEnd() works", () => {
+    expect(S.padEnd("foo", 3)).toBe("foo");
+    expect(S.padEnd("foo", 2)).toBe("foo");
+
+    expect(S.padEnd("foo", 5, ".")).toBe("foo..");
+    expect(S.padEnd("foo", 5, "123")).toBe("foo12");
+  });
+
+  test("truncateStart() works", () => {
+    expect(S.truncateStart("foo", 3)).toBe("foo");
+    expect(S.truncateStart("foo", 2)).toBe("oo");
+
+    expect(S.truncateStart("foo", 3, "...")).toBe("foo");
+    expect(S.truncateStart("aaaaaaaaaaaaaaa", 5, "...")).toBe("...aa");
+    expect(S.truncateStart("aaaaaaaaaaaaaaa", 5, "…")).toBe("…aaaa");
+
+    expect(() => S.truncateStart("foo", 2, "....")).toThrow(RangeError);
+  });
+
+  test("truncateEnd() works", () => {
+    expect(S.truncateEnd("foo", 3)).toBe("foo");
+    expect(S.truncateEnd("foo", 2)).toBe("fo");
+
+    expect(S.truncateEnd("foo", 3, "...")).toBe("foo");
+    expect(S.truncateEnd("aaaaaaaaaaaaaaa", 5, "...")).toBe("aa...");
+    expect(S.truncateEnd("aaaaaaaaaaaaaaa", 5, "…")).toBe("aaaa…");
+
+    expect(() => S.truncateEnd("foo", 2, "....")).toThrow(RangeError);
   });
 });
--- src/String.ts
+++ src/String.ts
@@ -1,7 +1,30 @@
+type TStringifiable = string | String | number | boolean | null | undefined;
+type TLooseStringInput =
+  | TStringifiable
+  | {
+      toString(): TStringifiable;
+    }
+  | {
+      [Symbol.toPrimitive](): TStringifiable;
+    };
+
 /**
  * The S class, for String, provides useful methods for working with strings.
  */
 class S extends String {
+  /**
+   * Converts any value to a string.
+   * `null` and `undefined` are converted to empty strings.
+   * Non-string values are converted using `String()`.
+   */
+  static from(str: TLooseStringInput): string {
+    if (str === null || str === undefined) {
+      return "";
+    }
+
+    return String(str);
+  }
+
   /**
    * A simple is-string check.
    * Returns true both for primitive strings and String objects.
@@ -16,6 +39,221 @@ class S extends String {
    */
   static isStrict(x: any): x is string {
     return typeof x === "string" && x !== "";
+  }
+
+  /**
+   * Capitalizes the first letter of a string.
+   */
+  static capitalize(str: TLooseStringInput): string {
+    let sane = S.from(str);
+    return sane.charAt(0).toUpperCase() + sane.slice(1);
+  }
+
+  /**
+   * Converts all the alphabetic characters in a string to lowercase.
+   */
+  static toLowerCase(str: TLooseStringInput): string {
+    return S.from(str).toLowerCase();
+  }
+
+  /**
+   * Converts all the alphabetic characters in a string to uppercase.
+   */
+  static toUpperCase(str: TLooseStringInput): string {
+    return S.from(str).toUpperCase();
+  }
+
+  /**
+   * Returns a string where all alphabetic characters have been converted to uppercase, taking into account the host environment's current locale.
+   */
+  static toLocaleUpperCase(
+    str: TLooseStringInput,
+    locales?: string | string[] | undefined
+  ): string {
+    return S.from(str).toLocaleUpperCase(locales);
+  }
+
+  /**
+   * Returns a string where all alphabetic characters have been converted to lowercase, taking into account the host environment's current locale.
+   */
+  static toLocaleLowerCase(
+    str: TLooseStringInput,
+    locales?: string | string[] | undefined
+  ): string {
+    return S.from(str).toLocaleLowerCase(locales);
+  }
+
+  /**
+   * Splits a string into an array of words.
+   * Considers all non-alphanumeric characters as well as capital letters as word boundaries.
+   * All non-alphanumeric characters are excluded from the result.
+   */
+  static splitWords(str: TLooseStringInput): string[] {
+    return S.from(str)
+      .split(/[^a-zA-Z0-9]+|(?=[A-Z])/g)
+      .filter(Boolean);
+  }
+
+  /**
+   * Converts a string to Title Case.
+   * It only splits the string by spaces.
+   *
+   * Since Title Case aims to be displayed
+   */
+  static toTitleCase(str: TLooseStringInput): string {
+    return S.from(str)
+      .split(/\s+/)
+      .map(S.capitalize)
+      .join(" ")
+      .split("-")
+      .map(S.capitalize)
+      .join("-");
+  }
+
+  /**
+   * Trims a string on both ends, removing the specified characters or pattern, or spaces by default.
+   * Warning: providing a string of multiple characters will remove all occurrences of each character, not the whole string.
+   */
+  static trim(str: TLooseStringInput, chars?: string | RegExp): string {
+    if (!chars) {
+      return S.from(str).trim();
+    }
+
+    if (S.is(chars)) {
+      return S.from(str).replace(
+        new RegExp(`^[${chars}]+|[${chars}]+$`, "g"),
+        ""
+      );
+    }
+
+    if (chars instanceof RegExp) {
+      return S.from(str).replace(
+        new RegExp(`^(${chars.source})+|(${chars.source})+$`, "g"),
+        ""
+      );
+    }
+
+    throw new TypeError(
+      "S.trim() only accepts strings or RegExp as second argument."
+    );
+  }
+
+  /**
+   * Trims a string on the left, removing the specified characters or pattern, or spaces by default.
+   * Warning: providing a string of multiple characters will remove all occurrences of each character, not the whole string.
+   */
+  static trimStart(str: TLooseStringInput, chars?: string | RegExp): string {
+    if (!chars) {
+      return S.from(str).trimStart();
+    }
+
+    if (S.is(chars)) {
+      return S.from(str).replace(new RegExp(`^[${chars}]+`, "g"), "");
+    }
+
+    if (chars instanceof RegExp) {
+      return S.from(str).replace(new RegExp(`^(${chars.source})+`, "g"), "");
+    }
+
+    throw new TypeError(
+      "S.trimStart() only accepts strings or RegExp as second argument."
+    );
+  }
+
+  /**
+   * Trims a string on the right, removing the specified characters or pattern, or spaces by default.
+   * Warning: providing a string of multiple characters will remove all occurrences of each character, not the whole string.
+   */
+  static trimEnd(str: TLooseStringInput, chars?: string | RegExp): string {
+    if (!chars) {
+      return S.from(str).trimEnd();
+    }
+
+    if (S.is(chars)) {
+      return S.from(str).replace(new RegExp(`[${chars}]+$`, "g"), "");
+    }
+
+    if (chars instanceof RegExp) {
+      return S.from(str).replace(new RegExp(`(${chars.source})+$`, "g"), "");
+    }
+
+    throw new TypeError(
+      "S.trimEnd() only accepts strings or RegExp as second argument."
+    );
+  }
+
+  /**
+   * Pads the left side of a string with the specified characters, or spaces by default, until the string reaches the specified length.
+   */
+  static padStart(
+    str: TLooseStringInput,
+    length: number,
+    filler?: TLooseStringInput
+  ): string {
+    return S.from(str).padStart(length, S.from(filler) || " ");
+  }
+
+  /**
+   * Pads the right side of a string with the specified characters, or spaces by default, until the string reaches the specified length.
+   */
+  static padEnd(
+    str: TLooseStringInput,
+    length: number,
+    filler?: TLooseStringInput
+  ): string {
+    return S.from(str).padEnd(length, S.from(filler) || " ");
+  }
+
+  /**
+   * Truncates the left side of a string to the specified length.
+   * If the string is longer than the specified length and an ellipsis string is provided, the overhanging characters are replaced by the ellipsis.
+   */
+  static truncateStart(
+    str: TLooseStringInput,
+    length: number,
+    ellipsis?: TLooseStringInput
+  ): string {
+    let sane = S.from(str);
+    let saneEllipsis = S.from(ellipsis);
+
+    if (sane.length <= length) {
+      return sane;
+    }
+
+    if (saneEllipsis.length >= length) {
+      throw new RangeError(
+        "S.truncateStart() requires the length of the ellipsis to be shorter than the maximum length of the string."
+      );
+    }
+
+    return (
+      saneEllipsis + sane.slice(sane.length - length + saneEllipsis.length)
+    );
+  }
+
+  /**
+   * Truncates the right side of a string to the specified length.
+   * If the string is longer than the specified length and an ellipsis string is provided, the overhanging characters are replaced by the ellipsis.
+   */
+  static truncateEnd(
+    str: TLooseStringInput,
+    length: number,
+    ellipsis?: TLooseStringInput
+  ): string {
+    let sane = S.from(str);
+    let saneEllipsis = S.from(ellipsis);
+
+    if (sane.length <= length) {
+      return sane;
+    }
+
+    if (saneEllipsis.length >= length) {
+      throw new RangeError(
+        "S.truncateEnd() requires the length of the ellipsis to be shorter than the maximum length of the string."
+      );
+    }
+
+    return sane.slice(0, length - saneEllipsis.length) + saneEllipsis;
   }
 }
 
