983c8981945d9917de353cb4efd446c78d4b3773
apademide <contact@apademide.ch>
2023-12-23 17:38:26 +0100

    meta: update license + add TODO

---
+++ .changeset/fifty-seahorses-explode.md
@@ -0,0 +1,5 @@
+---
+"@auaust/primitive-kit": patch
+---
+
+Fix license in package.json
--- src/Array.ts
+++ src/Array.ts
@@ -5,6 +5,30 @@ class RawA extends Array {
   constructor() {
     super();
   }
+
+  /**
+   * A.from() should be able to convert Set and Map to Array.
+   *
+   */
+
+  /**
+   * A.collapse() should remove empty keys from an array, i.e. A.collapse([,,,1,,,2,3]) should return [1,2,3].
+   * A cloned approach is to using Array.prototype.flat(0) which does exactly this (with 0 it actually doesn't flatten anything, but it removes empty keys).
+   * Don't know of a way that'd do it out of the box in place.
+   */
+
+  /**
+   * A.realLength() should return the length of an array without counting empty keys.
+   * A.realLength([,,,1,,,2,3]) should return 3.
+   */
+
+  /**
+   * A.clone() should clone an array recursively.
+   */
+
+  /**
+   * A.equals() should compare two arrays recursively.
+   */
 }
 
 const A = new Proxy(RawA, {
