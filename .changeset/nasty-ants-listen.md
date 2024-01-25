926d5585389b5dadd0615368c4ad61497c5f5aff
apademide <contact@apademide.ch>
2024-01-25 22:39:21 +0100

    style: remove `PrimitiveKit ` class + default export

---
+++ .changeset/nasty-ants-listen.md
@@ -0,0 +1,5 @@
+---
+"@auaust/primitive-kit": minor
+---
+
+No more default export and remove PrimitiveKit class
--- src/index.ts
+++ src/index.ts
@@ -6,39 +6,4 @@ import { O } from "~/Object";
 import { S } from "~/String";
 import { P } from "~/Primitives";
 
-/**
- * PrimitiveKit is a collection of classes that provide useful methods for working with primitive types.
- * It is advised to import the classes directly rather than through the properties of this class.
- */
-class PrimitiveKit {
-  static get A(): typeof A {
-    return A;
-  }
-
-  static get F(): typeof F {
-    return F;
-  }
-
-  static get B(): typeof B {
-    return B;
-  }
-
-  static get N(): typeof N {
-    return N;
-  }
-
-  static get O(): typeof O {
-    return O;
-  }
-
-  static get S(): typeof S {
-    return S;
-  }
-
-  static get P(): typeof P {
-    return P;
-  }
-}
-
-export default PrimitiveKit;
-export { A, B, N, O, P, PrimitiveKit, S };
+export { A, F, B, N, O, S, P };
