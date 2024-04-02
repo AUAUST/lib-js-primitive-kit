f22d97f47deb699b317e8d6919f997dbc323f68a
apademide <contact@apademide.ch>
2024-04-02 19:11:06 +0200

    meta: test new entry points

---
+++ .changeset/stale-horses-applaud.md
@@ -0,0 +1,5 @@
+---
+"@auaust/primitive-kit": minor
+---
+
+Test a new structure for entry points
--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,51 @@
-import { readFileSync } from "fs";
-import { dirname, resolve } from "path";
+import { builtinModules } from "module";
+import { basename, resolve } from "path";
 import { cwd } from "process";
 
 import terser from "@rollup/plugin-terser";
 import ts from "rollup-plugin-ts";
 
+import pkg from "./package.json" assert { type: "json" };
+
 const currDir = cwd();
-
-const pkg = (() => {
-  function findPkg(start = currDir, level = 0) {
-    try {
-      const path = resolve(start, "package.json");
-      const content = readFileSync(path, { encoding: "utf8" });
-      return JSON.parse(content);
-    } catch {
-      return level >= 10 ? {} : findPkg(dirname(start), level + 1);
-    }
-  }
-
-  return findPkg();
-})();
-
 const outDir = resolve(currDir, "dist");
 
+const entries = {
+  index: "src/index.ts",
+  arrays: "src/arrays/methods.ts",
+  booleans: "src/booleans/methods.ts",
+  functions: "src/functions/methods.ts",
+  numbers: "src/numbers/methods.ts",
+  objects: "src/objects/methods.ts",
+  primitives: "src/primitives/methods.ts",
+  strings: "src/strings/methods.ts",
+};
+
 export default {
-  input: "src/index.ts",
+  input: entries,
   output: [
     {
-      file: resolve(outDir, "cjs", "index.js"),
+      dir: resolve(outDir, "cjs"),
       format: "cjs",
-      sourcemap: true,
     },
     {
-      file: resolve(outDir, "esm", "index.js"),
+      dir: resolve(outDir, "esm"),
       format: "esm",
       sourcemap: true,
     },
   ],
   external: [
+    ...builtinModules,
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
     ts({
-      transpiler: {
-        typescriptSyntax: "typescript",
-        otherSyntax: "babel",
-      },
-      browserslist: false,
       hook: {
-        // Always rename declaration files to index.d.ts to avoid emitting two declaration files with identical contents
         outputPath: (path, kind) =>
-          kind === "declaration" ? "./dist/index.d.ts" : path,
+          kind === "declaration"
+            ? resolve(outDir, "types", basename(path))
+            : path,
       },
     }),
     terser(),
