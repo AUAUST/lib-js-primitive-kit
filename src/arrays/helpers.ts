233a6384387da04a3ab91d2ed411ea56260ad269
apademide <contact@apademide.ch>
2024-12-21 21:30:15 +0100

    imp: `ExpectedArrayError` extends `TypeError`

--- src/arrays/helpers.ts
+++ src/arrays/helpers.ts
@@ -1,5 +1,5 @@
 /** An error that's thrown when an array-helper function is given a non-array value. */
-export class ExpectedArrayError extends Error {
+export class ExpectedArrayError extends TypeError {
   constructor() {
     super("Expected an array object.");
   }
