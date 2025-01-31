b8031b55c5a46534f9b68cc5cd56afae6f95d5e7
apademide <contact@apademide.ch>
2025-01-31 16:28:30 +0100

    fix: type level issue

--- src/tests/Array.test.ts
+++ src/tests/Array.test.ts
@@ -631,7 +631,7 @@ describe("A class", () => {
       expect(output).toEqual([1, 2, 3, 4, [5, 6]]);
       expect(output).not.toBe(input);
 
-      type Test = Expect<Equal<typeof output, number[]>>;
+      type Test = Expect<Equal<typeof output, (number | number[])[]>>;
     }
 
     {
