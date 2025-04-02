4601cd9290b8bef9189324417693ced22eaeb660
apademide <contact@apademide.ch>
2025-04-02 14:32:37 +0200

    imp: `randomInteger` ranges from `0` to `1` by default
    This is a breaking change.

--- src/numbers/methods/randomInteger.ts
+++ src/numbers/methods/randomInteger.ts
@@ -3,7 +3,7 @@ import { toNumber } from "./toNumber";
 
 export function randomInteger(
   min: Numberifiable = 0,
-  max: Numberifiable = 100
+  max: Numberifiable = 1
 ): number {
   const saneMin = toNumber(min);
   return Math.floor(Math.random() * (toNumber(max) - saneMin + 1)) + saneMin;
