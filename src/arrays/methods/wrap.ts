bd672bbe1b6a66e370833c871ec7d64ac9f19a0e
apademide <contact@apademide.ch>
2025-05-18 22:56:57 +0200

    fix: handle readonly arrays in `A.wrap`

--- src/arrays/methods/wrap.ts
+++ src/arrays/methods/wrap.ts
@@ -3,7 +3,9 @@ import { isArray } from "./isArray";
 // `unknown[]` is a more convenient return type than `[]`
 // as it allows further working with the array.
 export function wrap(value?: null | undefined | never): unknown[];
-export function wrap<T>(value: T | null | undefined): T extends any[] ? T : T[];
+export function wrap<T>(
+  value: T | null | undefined
+): T extends readonly any[] ? T : T[];
 export function wrap(value: unknown): unknown[] {
   if (value === undefined || value === null) {
     return [];
