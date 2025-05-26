5c01900d5c2e491b2dca9b16770881a4b236b6d4
apademide <contact@apademide.ch>
2025-05-26 11:15:05 +0200

    tests: moving tests to individual files
    Towards #123

--- tests/functions/tryCatch.test.ts
+++ tests/functions/tryCatch.test.ts
@@ -1,9 +1,36 @@
 import { tryCatch } from "@auaust/primitive-kit/functions";
 
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vitest } from "vitest";
 
 describe("tryCatch()", () => {
-  it("should work", () => {
-    expect(tryCatch).toBeTypeOf("function");
+  it("should forward the arguments to the function", () => {
+    const fn = vitest.fn((...args: number[]) => {
+      return args.reduce((a, b) => a + b, 0);
+    });
+
+    expect(tryCatch(fn, undefined, 1, 2, 3, 4)).toBe(10);
+    expect(fn).toHaveBeenLastCalledWith(1, 2, 3, 4);
+  });
+
+  it("should return the result of the function when it does not throw", () => {
+    expect(tryCatch(() => 1)).toBe(1);
+    expect(tryCatch(() => undefined)).toBe(undefined);
+    expect(tryCatch(() => false)).toBe(false);
+    expect(tryCatch(() => new Error())).toBeInstanceOf(Error);
+    expect(tryCatch(() => {})).toBe(undefined);
+  });
+
+  it("should return the fallback value when the function throws", () => {
+    expect(
+      tryCatch(() => {
+        throw new Error();
+      })
+    ).toBe(undefined);
+
+    expect(
+      tryCatch(() => {
+        throw new Error();
+      }, 1)
+    ).toBe(1);
   });
 });
--- tests/functions/tryCatchAsync.test.ts
+++ tests/functions/tryCatchAsync.test.ts
@@ -1,9 +1,36 @@
 import { tryCatchAsync } from "@auaust/primitive-kit/functions";
 
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vitest } from "vitest";
 
 describe("tryCatchAsync()", () => {
-  it("should work", () => {
-    expect(tryCatchAsync).toBeTypeOf("function");
+  it("should forward the arguments to the function", async () => {
+    const fn = vitest.fn(async (...args: number[]) => {
+      return args.reduce((a, b) => a + b, 0);
+    });
+
+    expect(await tryCatchAsync(fn, undefined, 1, 2, 3, 4)).toBe(10);
+    expect(fn).toHaveBeenLastCalledWith(1, 2, 3, 4);
+  });
+
+  it("should resolve with the result of the function when it does not throw", async () => {
+    expect(await tryCatchAsync(async () => 1)).toBe(1);
+    expect(await tryCatchAsync(async () => undefined)).toBe(undefined);
+    expect(await tryCatchAsync(async () => false)).toBe(false);
+    expect(await tryCatchAsync(async () => new Error())).toBeInstanceOf(Error);
+    expect(await tryCatchAsync(async () => {})).toBe(undefined);
+  });
+
+  it("should resolve with the fallback value when the function throws", async () => {
+    expect(
+      await tryCatchAsync(async () => {
+        throw new Error();
+      })
+    ).toBe(undefined);
+
+    expect(
+      await tryCatchAsync(async () => {
+        throw new Error();
+      }, 1)
+    ).toBe(1);
   });
 });
