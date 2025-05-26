7e359efc66a0ba5ad0ba9474e28ea34569f6ce16
apademide <contact@apademide.ch>
2025-05-26 14:59:08 +0200

    fix: `toString()` calls `valueOf()` if no custom `toString()` exists

--- src/strings/methods/toString.ts
+++ src/strings/methods/toString.ts
@@ -7,5 +7,13 @@ export function toString(str: unknown): string {
     return "";
   }
 
+  // If the passed object's `toString()` method is the one from `Object.prototype`,
+  // this means no custom `toString()` method is defined and the string will be
+  // in the format `[object Object]`. In this case, we call `valueOf()` first
+  //  which usually provides a more meaningful representation of the object.
+  if (str.toString === Object.prototype.toString) {
+    str = str.valueOf();
+  }
+
   return String(str);
 }
