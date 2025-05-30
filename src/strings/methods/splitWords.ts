b32dedba7c47467c500deb55983c4dc0f2e67b62
apademide <contact@apademide.ch>
2025-05-30 14:18:15 +0200

    cq: better explain the scary regex

--- src/strings/methods/splitWords.ts
+++ src/strings/methods/splitWords.ts
@@ -3,15 +3,33 @@ import type { Stringifiable } from "~/strings/types";
 import { toString } from "./toString";
 import { unaccent } from "./unaccent";
 
+/**
+ * This regex will match all non-alphanumeric characters,
+ * using `\p{L}` which matches any letter in any language,
+ * including accented characters and non-Latin scripts.
+ * It also uses `\d` to match any digit.
+ */
+const NON_ALPHANUMERIC_REGEX = /[^\p{L}\d]+/gu;
+
+/**
+ * This regex will match all non-alphanumeric characters
+ * using the same approach as above, but it will also match
+ * characters directly followed by a capital letter using
+ * lookahead assertion `(?=[\p{Lu}])`.
+ */
+const NON_ALPHANUMERIC_OR_CHARACTER_BEFORE_CAPITAL_REGEX =
+  /[^\p{L}\d]+|(?=[\p{Lu}])/gu;
+
 export function splitWords(
   str: Stringifiable,
   options?: CasingOptions
 ): string[] {
   const { ignoreCaps, unaccent: unaccented } = casingOptions(options);
-  const s = unaccented ? unaccent(str) : toString(str);
-  const regex = ignoreCaps
-    ? /[^\p{L}\d]+/gu // will match all non-alphanumeric characters
-    : /[^\p{L}\d]+|(?=[\p{Lu}])/gu; // will match all non-alphanumeric characters AND positions before a capital letter
+  const string = unaccented ? unaccent(str) : toString(str);
 
-  return s.split(regex).filter(Boolean);
+  const regex = ignoreCaps
+    ? NON_ALPHANUMERIC_REGEX
+    : NON_ALPHANUMERIC_OR_CHARACTER_BEFORE_CAPITAL_REGEX;
+
+  return string.split(regex).filter(Boolean);
 }
